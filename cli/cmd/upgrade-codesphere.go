5c800ad2e54bdbdc8a21e97800dd965d4139e1a1
Manuel Dewald <manuel@codesphere.com>
2025-05-22 13:07:46 +0200

    chore(org): Update project setup (#6)

---
+++ cli/cmd/upgrade-codesphere.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// UpgradeCodesphereCmd represents the codesphere command
+type UpgradeCodesphereCmd struct {
+	cmd *cobra.Command
+}
+
+func (c *UpgradeCodesphereCmd) RunE(_ *cobra.Command, args []string) error {
+	//Command execution goes here
+
+	fmt.Printf("running %s", c.cmd.Use)
+
+	return nil
+}
+
+func AddUpgradeCodesphereCmd(upgrade *cobra.Command) {
+	codesphere := UpgradeCodesphereCmd{
+		cmd: &cobra.Command{
+			Use:   "codesphere",
+			Short: "A brief description of your command",
+			Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+		},
+	}
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// codesphere.cmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// codesphere.cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	upgrade.AddCommand(codesphere.cmd)
+	codesphere.cmd.RunE = codesphere.RunE
+
+	// Add child commands here
+	// AddCodesphereChildCmd(codesphere.cmd)
+}
+
---
+++ cli/cmd/upgrade.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// UpgradeCmd represents the upgrade command
+type UpgradeCmd struct {
+	cmd *cobra.Command
+}
+
+func (c *UpgradeCmd) RunE(_ *cobra.Command, args []string) error {
+	//Command execution goes here
+
+	fmt.Printf("running %s", c.cmd.Use)
+
+	return nil
+}
+
+func AddUpgradeCmd(rootCmd *cobra.Command) {
+	upgrade := UpgradeCmd{
+		cmd: &cobra.Command{
+			Use:   "upgrade",
+			Short: "A brief description of your command",
+			Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+		},
+	}
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// upgrade.cmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// upgrade.cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(upgrade.cmd)
+	upgrade.cmd.RunE = upgrade.RunE
+
+	// Add child commands here
+	// AddUpgradeChildCmd(upgrade.cmd)
+}
+
