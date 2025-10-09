88db295f27d9481affa0ec6998f8a41fa925c94e
OliverTrautvetter <66372584+OliverTrautvetter@users.noreply.github.com>
2025-10-09 13:25:10 +0200

    fix: ApiKey struct fields with correct json key

--- internal/portal/api_key.go
+++ internal/portal/api_key.go
@@ -3,11 +3,12 @@ package portal
 import "time"
 
 type ApiKey struct {
-	RID          string    `json:"rid"`
-	ApiKey       string    `json:"apiKey"`
+	KeyID        string    `json:"keyId"`
 	Owner        string    `json:"owner"`
 	Organization string    `json:"organization"`
 	Role         string    `json:"role"`
 	CreatedAt    time.Time `json:"createdAt"`
 	ExpiresAt    time.Time `json:"expiresAt"`
+	// Temp
+	ApiKey string `json:"apiKey"`
 }
