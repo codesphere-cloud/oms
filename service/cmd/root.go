ddd9ae23a04d05c3336b59b6179cadb6f9bb48f6
Manuel Dewald <manuel@codesphere.com>
2025-10-09 16:38:18 +0200

    add service description

--- service/cmd/root.go
+++ service/cmd/root.go
@@ -1,43 +1,22 @@
 // Copyright (c) Codesphere Inc.
 // SPDX-License-Identifier: Apache-2.0
-
 package cmd
 
 import (
 	"os"
 
+	"github.com/codesphere-cloud/cs-go/pkg/io"
 	"github.com/spf13/cobra"
 )
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	// rootCmd represents the base command when called without any subcommands
 	rootCmd := &cobra.Command{
 		Use:   "service",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+		Short: "Codesphere Operations Management System",
+		Long: io.Long(`This is the OMS standalone service, which can be used to manage and observe Codesphere installations.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-		// Uncomment the following line if your bare application
-		// has an action associated with it:
-		// Run: func(cmd *cobra.Command, args []string) { },
+			This area is work in progress! OMS is under heavy development so please take a look back soon!`),
 	}
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.service.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-	// Add child commands here
-	// AddChildCmd(rootCmd)
 
 	err := rootCmd.Execute()
 	if err != nil {
