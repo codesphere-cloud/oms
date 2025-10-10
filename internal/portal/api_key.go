8324c7ce864ffea638d6e1cf69aba8e60fe440ab
OliverTrautvetter <66372584+OliverTrautvetter@users.noreply.github.com>
2025-10-10 09:30:30 +0200

    feat: List api key (#30)
    Signed-off-by: OliverTrautvetter <66372584+OliverTrautvetter@users.noreply.github.com>

--- internal/portal/api_key.go
+++ internal/portal/api_key.go
@@ -3,11 +3,12 @@ package portal
 import "time"
 
 type ApiKey struct {
-	RID          string    `json:"rid"`
-	ApiKey       string    `json:"api_key"`
+	KeyID        string    `json:"key_id"`
 	Owner        string    `json:"owner"`
 	Organization string    `json:"organization"`
 	Role         string    `json:"role"`
 	CreatedAt    time.Time `json:"createdAt"`
 	ExpiresAt    time.Time `json:"expiresAt"`
+	// Temp
+	ApiKey string `json:"api_key"`
 }
