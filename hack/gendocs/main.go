e3e47e84792d978917d8b12b7950d60c89910ca0
OliverTrautvetter <66372584+OliverTrautvetter@users.noreply.github.com>
2025-10-16 09:42:51 +0200

    fix: prevent temp path in docs

--- hack/gendocs/main.go
+++ hack/gendocs/main.go
@@ -5,12 +5,16 @@ package main
 
 import (
 	"log"
+	"os"
 
 	oms "github.com/codesphere-cloud/oms/cli/cmd"
 	"github.com/spf13/cobra/doc"
 )
 
 func main() {
+	// to ensure the stable binary name is used and not the temporary path
+	os.Args[0] = "oms"
+
 	err := doc.GenMarkdownTree(oms.GetRootCmd(), "docs")
 	if err != nil {
 		log.Fatal(err)
