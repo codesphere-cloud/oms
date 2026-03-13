156f98748bf9806548e8b66bcde943487a07eba2
Tim Schrodi <tim@codesphere.com>
2026-03-13 09:56:30 +0100

    add missing files
    Signed-off-by: Tim Schrodi <tim@codesphere.com>

---
+++ internal/util/testing/utils.go
@@ -0,0 +1,17 @@
+package testing
+
+import (
+	"github.com/onsi/gomega"
+)
+
+func MustMap[T any](value any) map[string]T {
+	result, ok := value.(map[string]T)
+	gomega.Expect(ok).To(gomega.BeTrue(), "expected map[string]%T, got %T", *new(T), value)
+	return result
+}
+
+func AssertZeroRequests(value any) {
+	requests := MustMap[int](value)
+	gomega.Expect(requests).To(gomega.HaveKeyWithValue("cpu", 0))
+	gomega.Expect(requests).To(gomega.HaveKeyWithValue("memory", 0))
+}
