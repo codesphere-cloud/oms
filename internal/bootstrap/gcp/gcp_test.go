730254616aebb4fe3c016e3bf7b21787db45aa77
Jonas Kauke <jonas.kauke@codesphere.com>
2026-03-18 11:25:57 +0100

    chore: debug workflow

--- internal/bootstrap/gcp/gcp_test.go
+++ internal/bootstrap/gcp/gcp_test.go
@@ -41,6 +41,8 @@ var _ = Describe("GCP Bootstrapper", func() {
 		ctx        context.Context
 		e          env.Env
 
+		projectLabels map[string]string
+
 		icg              *installer.MockInstallConfigManager
 		gc               *gcp.MockGCPClientManager
 		fw               *util.MockFileIO
@@ -109,6 +111,8 @@ var _ = Describe("GCP Bootstrapper", func() {
 			ControlPlaneNodes: []*node.Node{fakeNode("k0s-1", nodeClient), fakeNode("k0s-2", nodeClient), fakeNode("k0s-3", nodeClient)},
 			CephNodes:         []*node.Node{fakeNode("ceph-1", nodeClient), fakeNode("ceph-2", nodeClient), fakeNode("ceph-3", nodeClient), fakeNode("ceph-4", nodeClient)},
 		}
+
+		projectLabels = map[string]string{}
 	})
 	Describe("NewGCPBootstrapper", func() {
 		It("creates a valid GCPBootstrapper", func() {
@@ -151,7 +155,7 @@ var _ = Describe("GCP Bootstrapper", func() {
 			// 3. EnsureProject
 			gc.EXPECT().GetProjectByName(mock.Anything, "test-project").Return(nil, fmt.Errorf("project not found: test-project"))
 			gc.EXPECT().CreateProjectID("test-project").Return(projectId)
-			gc.EXPECT().CreateProject(mock.Anything, mock.Anything, "test-project", mock.Anything).Return(mock.Anything, nil)
+			gc.EXPECT().CreateProject(mock.Anything, mock.Anything, "test-project", projectLabels).Return(mock.Anything, nil)
 
 			// 4. EnsureBilling
 			gc.EXPECT().GetBillingInfo(projectId).Return(&cloudbilling.ProjectBillingInfo{BillingEnabled: false}, nil)
@@ -505,7 +509,7 @@ var _ = Describe("GCP Bootstrapper", func() {
 			It("creates project when missing", func() {
 				gc.EXPECT().GetProjectByName(csEnv.FolderID, csEnv.ProjectName).Return(nil, fmt.Errorf("project not found: %s", csEnv.ProjectName))
 				gc.EXPECT().CreateProjectID(csEnv.ProjectName).Return("new-proj-id")
-				gc.EXPECT().CreateProject(csEnv.FolderID, "new-proj-id", csEnv.ProjectName, mock.Anything).Return("", nil)
+				gc.EXPECT().CreateProject(csEnv.FolderID, "new-proj-id", csEnv.ProjectName, map[string]string{}).Return("", nil)
 
 				err := bs.EnsureProject()
 				Expect(err).NotTo(HaveOccurred())
@@ -525,7 +529,7 @@ var _ = Describe("GCP Bootstrapper", func() {
 			It("returns error when CreateProject fails", func() {
 				gc.EXPECT().GetProjectByName("", csEnv.ProjectName).Return(nil, fmt.Errorf("project not found: %s", csEnv.ProjectName))
 				gc.EXPECT().CreateProjectID(csEnv.ProjectName).Return("fake-id")
-				gc.EXPECT().CreateProject("", "fake-id", csEnv.ProjectName, mock.Anything).Return("", fmt.Errorf("create error"))
+				gc.EXPECT().CreateProject("", "fake-id", csEnv.ProjectName, map[string]string{}).Return("", fmt.Errorf("create error"))
 
 				err := bs.EnsureProject()
 				Expect(err).To(HaveOccurred())
