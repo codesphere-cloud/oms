67fe13b4f5e379604a35a5f9cca7f5c63dd66218
Jonas Kauke <jonas.kauke@codesphere.com>
2026-03-26 10:47:28 +0100

    refactor: split iam and admin related functions into new file

--- internal/bootstrap/gcp/iam_admin.go
+++ internal/bootstrap/gcp/iam_admin.go
@@ -1,3 +1,6 @@
+// Copyright (c) Codesphere Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 package gcp
 
 import (
@@ -52,6 +55,23 @@ func (b *GCPBootstrapper) EnsureProject() error {
 	}
 
 	return fmt.Errorf("failed to get project: %w", err)
+}
+
+// calculateProjectExpiryLabel takes a TTL string (e.g. "24h") and
+// returns a formatted UTC timestamp string that is usable as a GCP project label for automatic deletion.
+func calculateProjectExpiryLabel(projectTTLStr string) (string, error) {
+	projectTTL, err := time.ParseDuration(projectTTLStr)
+	if err != nil {
+		return "", fmt.Errorf("invalid project TTL format: %w", err)
+	}
+
+	// prepare label for gcp project deletion in custom UTC time format.
+	// GCP Labels are very limited. This is an easy way to add date and TZ info in one label.
+	gcpExpiryLabelLayout := "2006-01-02_15-04-05"
+	deleteProjectAfter := time.Now().UTC().Add(projectTTL).Format(gcpExpiryLabelLayout)
+	deleteProjectAfter = fmt.Sprintf("%s_utc", deleteProjectAfter)
+
+	return deleteProjectAfter, nil
 }
 
 func (b *GCPBootstrapper) EnsureBilling() error {
