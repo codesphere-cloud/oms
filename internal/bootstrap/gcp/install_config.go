e19628ecf2ef27076946612370155e09910ed961
OliverTrautvetter <66372584+OliverTrautvetter@users.noreply.github.com>
2026-04-10 10:10:45 +0200

    feat: update PostgreSQL hostname in install config and preserve vault-only secrets in WriteVault

--- internal/bootstrap/gcp/install_config.go
+++ internal/bootstrap/gcp/install_config.go
@@ -117,6 +117,7 @@ func (b *GCPBootstrapper) UpdateInstallConfig() error {
 	previousPrimaryIP := b.Env.InstallConfig.Postgres.Primary.IP
 	previousPrimaryHostname := b.Env.InstallConfig.Postgres.Primary.Hostname
 	b.Env.InstallConfig.Postgres.Primary.IP = b.Env.PostgreSQLNode.GetInternalIP()
+	b.Env.InstallConfig.Postgres.Primary.Hostname = b.Env.PostgreSQLNode.GetName()
 
 	b.Env.InstallConfig.Ceph.CsiKubeletDir = "/var/lib/k0s/kubelet"
 	b.Env.InstallConfig.Ceph.NodesSubnet = "10.10.0.0/20"
